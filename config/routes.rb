cd12a25a64a2ee24082b61fc17e34865d4a9bf90
Dominik Schiekofer <schiekofer.d@gmail.com>
2025-11-25 13:16:15 +0100

    first round of routes created

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,17 @@ Rails.application.routes.draw do
       }
 
   root to: "pages#home"
+
+  resources :decks, only: [:index, :show, :new, :create] do
+    resources :messages, only: [:new, :create]
+    resources :cards, only: [:index]
+  end
+
+  resources :user_decks, only: [:index, :show, :create, :destroy] do
+    resources :user_cards, only: [:show, :update]
+  end
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
