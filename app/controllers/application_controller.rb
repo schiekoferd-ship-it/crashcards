448c89041130041658ee229e498977a0f72bd6e1
Christian Lange-Hausstein <langehausstein@gmail.com>
2025-11-26 16:45:17 +0100

    Add homepage and redirect after login to decks#index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  # After successful login, send user to decks#index
+  def after_sign_in_path_for(resource)
+    decks_path
+  end
 end
