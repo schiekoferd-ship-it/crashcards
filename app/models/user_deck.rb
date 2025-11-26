2f5437606cf2173dc3425828b62b3727413b501f
Antonius Lambsdorff <lambsdorffa@gmail.com>
2025-11-26 17:52:44 +0100

    small changes to user-decks; more incoming

--- app/models/user_deck.rb
+++ app/models/user_deck.rb
@@ -3,8 +3,8 @@ class UserDeck < ApplicationRecord
   belongs_to :deck
   has_many :user_cards, dependent: :destroy
 
-  def progress_percent
-    learned = user_cards.where(status: true).count
-    ((learned.to_f / user_cards.count) * 100).round
-  end
+  # def progress_percent
+  #   learned = user_cards.where(status: true).count
+  #   ((learned.to_f / user_cards.count) * 100).round
+  # end
 end
